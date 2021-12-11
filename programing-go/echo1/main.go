79a7e69ad3c718ff3102410e4767ae2b3f210afe
s-beats <sakashin10291029@gmail.com>
2021-12-12 08:03:10 +0900

    fix

--- programing-go/echo1/main.go
+++ programing-go/echo1/main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var s, sep string
-	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args, " "))
 }
