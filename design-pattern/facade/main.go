0a18361bc648de5b4b9fbb27bec03db21b829c39
s-beats <sakashin10291029@gmail.com>
2022-08-05 07:47:32 +0900

    facade pattern

--- design-pattern/facade/main.go
+++ design-pattern/facade/main.go
@@ -1,3 +1,29 @@
 package main
 
-// TODO: implement
+import "fmt"
+
+// 複数のクラスを使う処理を一つのインターフェースにまとめる
+
+type NotificationEmail struct{}
+
+type User struct {
+	ID    string
+	Email string
+}
+
+type EmailClient struct{}
+
+func (e *EmailClient) Send(email string) {
+	fmt.Println("Send email to", email)
+}
+
+func getUser(id string) *User {
+	return &User{
+		ID:    id,
+		Email: "test@example.com",
+	}
+}
+
+func (n *NotificationEmail) Send(userID string) {
+	(&EmailClient{}).Send(getUser(userID).Email)
+}
