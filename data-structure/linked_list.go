8ffae981549c815f39be45a1f33adac84f2d7d61
s-beats <sakashin10291029@gmail.com>
2022-08-05 21:55:09 +0900

    list interfacee

--- data-structure/linked_list.go
+++ data-structure/linked_list.go
@@ -3,18 +3,24 @@ package main
 // 要素とポインタからなる数珠繋ぎのリスト
 // 連続した領域にならないので事前にメモリ確保が不要
 // 頻繁にデータを書き換えるのならば有効
+
+type ListIF interface {
+	// 先頭に要素を追加する
+	PushHead(e Node[any])
+	// 末尾に要素を追加する
+	PushTail(e Node[any])
+	// 先頭の要素を取り出す
+	PopHead() Node[any]
+	// 末尾の要素を取り出す
+	PopTail() Node[any]
+	// 要素数を取得する
+	Len() int
+}
+
 type List[T any] struct {
 	head *Node[T]
 	tail *Node[T]
 	size int
-}
-
-func (l *List[T]) Add() {
-	// TODO
-}
-
-func (l *List[T]) Remove() {
-	// TODO
 }
 
 type Node[T any] struct {
