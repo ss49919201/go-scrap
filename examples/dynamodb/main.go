91c867a3e9f8a7df9e42ee3a26c25955fe9c2d50
s-beats <sakashin10291029@gmail.com>
2022-08-09 21:02:39 +0900

    guregu dynamo

---
+++ examples/dynamodb/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+// Use struct tags much like the standard JSON library,
+// you can embed anonymous structs too!
+type widget struct {
+	UserID int       // Hash key, a.k.a. partition key
+	Time   time.Time // Range key, a.k.a. sort key
+
+	Msg       string              `dynamo:"Message"`    // Change name in the database
+	Count     int                 `dynamo:",omitempty"` // Omits if zero value
+	Children  []widget            // Lists
+	Friends   []string            `dynamo:",set"` // Sets
+	Set       map[string]struct{} `dynamo:",set"` // Map sets
+	SecretKey string              `dynamo:"-"`    // Ignored
+}
+
+func main() {
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &aws.Config{Region: aws.String("us-west-2")})
+
+	w, err := getWidget(db, 1, time.Now())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(w)
+}
+
+func getWidget(db *dynamo.DB, id int, t time.Time) (*widget, error) {
+	table := db.Table("Widgets")
+	var result widget
+	if err := table.Get("UserID", id).
+		Range("Time", dynamo.Equal, t).
+		One(&result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
--- go.mod
+++ go.mod
@@ -14,15 +14,19 @@ require (
 	github.com/Code-Hex/dd v1.1.0
 	github.com/aws/aws-lambda-go v1.32.1
 	github.com/bxcodec/faker/v3 v3.8.0
+	github.com/guregu/dynamo v1.15.1
+	github.com/samber/do v1.2.0
 	github.com/samber/lo v1.26.0
 	github.com/samber/mo v1.0.1
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/samber/do v1.2.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 )
