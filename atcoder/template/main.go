fee1f07d7a4096708c45ba77a75093cd7511b571
s-beats <sakashin10291029@gmail.com>
2022-08-15 12:34:10 +0900

    atcoder template

---
+++ atcoder/template/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanText() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func toIntList(s []string) []int {
+	ns := make([]int, len(s))
+	for i, v := range s {
+		ns[i] = toInt(v)
+	}
+	return ns
+}
+
+func main() {
+	N := scanText()
+	ss := strings.Split(N, " ")
+	h1 := toInt(ss[0])
+	_ = toInt(ss[1])
+	as := make([][]int, h1)
+	for i := 0; i < h1; i++ {
+		N := scanText()
+		ss := strings.Split(N, " ")
+		as[i] = toIntList(ss)
+	}
+
+	N2 := scanText()
+	ss2 := strings.Split(N2, " ")
+	h2 := toInt(ss2[0])
+	_ = toInt(ss2[1])
+	bs := make([][]int, h2)
+	for i := 0; i < h2; i++ {
+		N := scanText()
+		ss := strings.Split(N, " ")
+		bs[i] = toIntList(ss)
+	}
+	fmt.Println(as, bs)
+}
