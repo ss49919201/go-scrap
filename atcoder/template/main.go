5e11f39df8795a86303331beb6fbb4e4762d2c18
s-beats <sakashin10291029@gmail.com>
2022-08-19 21:46:34 +0900

    is prime

--- atcoder/template/main.go
+++ atcoder/template/main.go
@@ -32,21 +32,6 @@ func isPrime(n int) bool {
 	if n < 2 {
 		return false
 	}
-	if n == 2 {
-		return true
-	}
-	for i := 2; i*i < n; i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func isPrimeDash(n int) bool {
-	if n < 2 {
-		return false
-	}
 	for i := 2; i*i <= n; i++ {
 		if n%i == 0 {
 			return false
