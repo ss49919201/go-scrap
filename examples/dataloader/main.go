48fb92fa6c84cd379d0273e6c3110fd46f9ff68a
s-beats <sakashin10291029@gmail.com>
2022-08-21 20:26:37 +0900

    user cache

--- examples/dataloader/main.go
+++ examples/dataloader/main.go
@@ -2,74 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"sync"
 
 	"github.com/graph-gophers/dataloader/v7"
 	lru "github.com/hashicorp/golang-lru"
 )
-
-func init() {
-	c, _ := lru.NewARC(100)
-	cache = &userCache[int, *User]{ARCCache: c}
-}
-
-var (
-	cache *userCache[int, *User]
-)
-
-type userCache[K comparable, V any] struct {
-	*lru.ARCCache
-}
-
-// Get gets an item from the cache
-func (c *userCache[K, V]) Get(_ context.Context, key K) (dataloader.Thunk[V], bool) {
-	v, ok := c.ARCCache.Get(key)
-	if ok {
-		return v.(dataloader.Thunk[V]), ok
-	}
-	return nil, ok
-}
-
-// Set sets an item in the cache
-func (c *userCache[K, V]) Set(_ context.Context, key K, value dataloader.Thunk[V]) {
-	c.ARCCache.Add(key, value)
-}
-
-// Delete deletes an item in the cache
-func (c *userCache[K, V]) Delete(_ context.Context, key K) bool {
-	if c.ARCCache.Contains(key) {
-		c.ARCCache.Remove(key)
-		return true
-	}
-	return false
-}
-
-// Clear cleasrs the cache
-func (c *userCache[K, V]) Clear() {
-	c.ARCCache.Purge()
-}
-
-var userData = map[int]*User{
-	1: {1},
-	2: {2},
-	3: {3},
-	4: {4},
-}
-
-type User struct {
-	ID int `json:"id"`
-}
-
-type UserRepo struct{}
-
-func (u *UserRepo) Find(id int) (*User, error) {
-	user, ok := userData[id]
-	if !ok {
-		return nil, fmt.Errorf("user not found")
-	}
-	return user, nil
-}
 
 func main() {
 	fn := func(ctx context.Context, keys []int) []*dataloader.Result[*User] {
@@ -86,7 +23,11 @@ func main() {
 		wg.Wait()
 		return results
 	}
+
+	c, _ := lru.NewARC(100)
+	cache := NewUserCache(WithUserCache[int, *User](c))
 	loader := dataloader.NewBatchedLoader(fn, dataloader.WithCache[int, *User](cache))
 
+	loader.Load(context.Background(), 1)()
 	loader.Load(context.Background(), 1)()
 }
