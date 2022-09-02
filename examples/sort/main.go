f05621cb1d178300b018890b998d86aec8c3e68e
s-beats <sakashin10291029@gmail.com>
2022-09-02 21:30:17 +0900

    copy slicee

--- examples/sort/main.go
+++ examples/sort/main.go
@@ -8,13 +8,30 @@ import (
 
 func main() {
 	a := []int{3, 2, 3, 0, 1, 3, 5, 9, 4, 10, 7, 8, 6}
+	b := make([]int, len(a))
+	copy(b, a)
 	quicksort(a)
+	quickSort(b, 0, len(b)-1)
 	fmt.Println(a)
+	fmt.Println(b)
 }
 
 func quickSort[T constraints.Ordered](arr []T, lo, hi int) {
+	partition := func(lo, hi int) int {
+		pivot := arr[hi]
+		pivotIndex := lo
+		for j := lo; j < hi; j++ {
+			if arr[j] < pivot {
+				arr[pivotIndex], arr[j] = arr[j], arr[pivotIndex]
+				pivotIndex++
+			}
+		}
+		arr[pivotIndex], arr[hi] = arr[hi], arr[pivotIndex]
+		return pivotIndex
+	}
+
 	if lo < hi {
-		p := partition(arr, lo, hi)
+		p := partition(lo, hi)
 		quickSort(arr, lo, p-1)
 		quickSort(arr, p+1, hi)
 	}
@@ -35,17 +52,4 @@ func quicksort[T constraints.Ordered](arr []T) {
 	arr[pivotIndex], arr[len(arr)-1] = pivot, arr[pivotIndex]
 	quicksort(arr[:pivotIndex])
 	quicksort(arr[pivotIndex+1:])
-}
-
-func partition[T constraints.Ordered](arr []T, lo, hi int) int {
-	pivot := arr[hi]
-	pivotIndex := lo
-	for j := lo; j < hi; j++ {
-		if arr[j] < pivot {
-			arr[pivotIndex], arr[j] = arr[j], arr[pivotIndex]
-			pivotIndex++
-		}
-	}
-	arr[pivotIndex], arr[hi] = arr[hi], arr[pivotIndex]
-	return pivotIndex
 }
