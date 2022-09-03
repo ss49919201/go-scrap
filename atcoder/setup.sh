bc55aec7bbb576bf3098470cdf6baedf9b47dd96
s-beats <sakashin10291029@gmail.com>
2022-09-03 19:40:03 +0900

    rename

--- atcoder/setup.sh
+++ atcoder/setup.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
 NOW=`date "+%Y%m%d"`
-MAIN_DIR=${1:-$NOW}
+DIR=${1:-$NOW}
 QESTION_TYPES=(a b c d)
 
 # 1回目のイテレートで親ディレクトリもまとめて作る
 for Q in ${QESTION_TYPES[@]}; do
-    TARGET=${MAIN_DIR}/${Q}
+    TARGET=${DIR}/${Q}
     mkdir -p ${TARGET}
     cp template/main.go ${TARGET}/main.go
 done
