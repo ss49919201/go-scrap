aa9c9ace87dc5ed56028189366c80dfee7010183
s-beats <sakashin10291029@gmail.com>
2022-09-04 20:32:17 +0900

    move

--- move_to_examples.sh
+++ move_to_examples.sh
@@ -1,14 +1,10 @@
 #/bin/bash
 
-# NOW=`date "+%Y%m%d"`
-# DIR=${1:-$NOW}
-FILES=`ls pattern`
+DIR=${1}
+FILES=`ls ${DIR}`
 
 # 1回目のイテレートで親ディレクトリもまとめて作る
 for FILE in ${FILES[@]}; do
-    # TARGET=${DIR}/${Q}
-    # mkdir -p ${TARGET}
-    # cp template/main.go ${TARGET}/main.go
     BASENAME=${FILE%.*}
     mkdir examples/${BASENAME}
     cp pattern/${FILE} examples/${BASENAME}/main.go
