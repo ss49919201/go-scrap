321c2b516b445efae5647a8af904f3f659f544c1
s-beats <sakashin10291029@gmail.com>
2022-09-10 23:35:12 +0900

    atcoder

---
+++ atcoder/abc288/a/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 1000000
+
+var rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			fmt.Println(e.Error())
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+type mem map[int]int
+
+func main() {
+	NS := readIntSlice()
+	var result int
+	m := make(mem)
+	for _, v := range NS {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		m[v] = 1
+		result++
+	}
+
+	fmt.Println(result)
+}
