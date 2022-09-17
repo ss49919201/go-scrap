2d5a6517b47c55a50f25ec93a86e3832656c6c42
s-beats <sakashin10291029@gmail.com>
2022-09-17 20:17:15 +0900

    script

--- atcoder/setup.sh
+++ atcoder/setup.sh
@@ -1,12 +1,20 @@
 #/bin/bash
 
+function create_dir() {
+    NOW=$1
+    DIR=$2
+    QESTION_TYPES=$3
+
+    # 1回目のイテレートで親ディレクトリもまとめて作る
+    for Q in ${QESTION_TYPES[@]}; do
+        TARGET=${DIR}/${Q}
+        mkdir -p ${TARGET}
+        cp template/main.go ${TARGET}/main.go
+    done
+}
+
 NOW=`date "+%Y%m%d"`
 DIR=${1:-$NOW}
 QESTION_TYPES=(a b c d)
 
-# 1回目のイテレートで親ディレクトリもまとめて作る
-for Q in ${QESTION_TYPES[@]}; do
-    TARGET=${DIR}/${Q}
-    mkdir -p ${TARGET}
-    cp template/main.go ${TARGET}/main.go
-done
+create_dir $NOW $DIR $QESTION_TYPES
