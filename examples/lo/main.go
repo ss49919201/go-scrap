c59abffbc9551a098433bdc3346a8acc294d159e
s-beats <sakashin10291029@gmail.com>
2022-09-17 10:09:22 +0900

    line

--- examples/lo/main.go
+++ examples/lo/main.go
@@ -13,9 +13,13 @@ func main() {
 
 func reduce() {
 	fmt.Println(
-		lo.Reduce([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, func(acc string, x, _ int) string {
-			acc += strconv.Itoa(x)
-			return acc
-		}, "This is the accumulator: "),
+		lo.Reduce(
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			func(acc string, x, _ int) string {
+				acc += strconv.Itoa(x)
+				return acc
+			},
+			"This is the accumulator: ",
+		),
 	)
 }
