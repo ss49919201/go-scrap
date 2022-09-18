2b3c5a2a163b8e960e52a3120826e9b15cff31d0
s-beats <sakashin10291029@gmail.com>
2022-09-18 13:35:46 +0900

    abc269

---
+++ atcoder/abc269/a/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 1000000
+
+var rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			fmt.Println(e.Error())
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func mod(n1, n2 int) int {
+	res := (n1 + n2) % n2
+	if res < 0 {
+		res += n2
+	}
+	return res
+}
+
+type mem map[int]int
+
+func main() {
+	NS := readIntSlice()
+
+	fmt.Println((NS[0] + NS[1]) * (NS[2] - NS[3]))
+	fmt.Println("Takahashi")
+}
---
+++ atcoder/abc269/b/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFSIZE = 1000000
+
+var rdr = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			fmt.Println(e.Error())
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func mod(n1, n2 int) int {
+	res := (n1 + n2) % n2
+	if res < 0 {
+		res += n2
+	}
+	return res
+}
+
+type mem map[int]int
+
+func main() {
+	NS := make([]string, 10)
+	ACDone := false
+
+	A, B, C, D := 0, 0, 0, 0
+	for i := 0; i < 10; i++ {
+		NS[i] = readline()
+		for k, v := range NS[i] {
+			if v == 35 {
+				if !ACDone {
+					A = i + 1
+					B = i + 1
+					C = k + 1
+					D = k + 1
+					ACDone = true
+				} else {
+					B = i + 1
+					D = k + 1
+				}
+			}
+		}
+	}
+
+	fmt.Println(A, B)
+	fmt.Println(C, D)
+}
