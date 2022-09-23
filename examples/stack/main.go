d7b513374459ffa1199f268914783a08ab8854e8
s-beats <sakashin10291029@gmail.com>
2022-09-23 12:02:17 +0900

    generic stack

--- examples/stack/main.go
+++ examples/stack/main.go
@@ -1,32 +1,50 @@
 package pattern
 
-import "errors"
+import (
+	"errors"
+	"reflect"
+)
 
-type Stack struct {
+type Stack[T any] struct {
 	sp   int
-	data []interface{}
+	data []T
 }
 
-func NewStack() *Stack {
-	return &Stack{
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
 		sp:   0,
-		data: []interface{}{},
+		data: []T{},
 	}
 }
 
-func (s *Stack) Pop() (interface{}, error) {
+func (s *Stack[T]) Pop() (T, error) {
 	if s.sp == 0 {
-		return nil, errors.New("stack is empty")
+		return zeroValue[T](), errors.New("stack is empty")
 	}
 	s.sp--
 	return s.data[s.sp], nil
 }
 
-func (s *Stack) Push(v interface{}) error {
-	if v == nil {
+func (s *Stack[T]) Push(v T) error {
+	if isNil(v) {
 		return errors.New("value is invalid")
 	}
 	s.data = append(s.data, v)
 	s.sp++
 	return nil
+}
+
+func zeroValue[T any]() T {
+	var zero T
+	return zero
+}
+
+func isNil(v any) bool {
+	rv := reflect.ValueOf(v)
+	switch rv.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Pointer, reflect.Slice, reflect.UnsafePointer:
+		return rv.IsNil()
+	default:
+		return false
+	}
 }
--- examples/url/main.go
+++ examples/url/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"path"
+	"strings"
 )
 
 func main() {
-	u, _ := url.JoinPath("http://example.com")
+	u, _ := url.JoinPath("http://example.com/../go")
+	p, _ := url.Parse("http://example.com/../go")
+	fmt.Println(p.Path)
+	fmt.Println(path.Join("../go/"))
 	fmt.Println(u) // http://example.com/
 
 	u2, _ := url.JoinPath("http://example.com", "/")
@@ -22,6 +27,7 @@ func main() {
 	fmt.Println(u5) // http://example.com/
 }
 
+// baseをurlとしてパースし、elemsをpathとしてurlに結合する
 func joinPath(base string, elems ...string) (s string, err error) {
 	u, err := url.Parse(base)
 	if err != nil {
@@ -30,3 +36,33 @@ func joinPath(base string, elems ...string) (s string, err error) {
 	s = u.JoinPath(elems...).String()
 	return
 }
+
+func parse(u string) {
+	// # より前と後ろに分ける
+	_, _, _ = strings.Cut(u, "#")
+
+	// # より前をパースする
+
+	// # より後ろがあればそれをセットする、なければそのまま返す
+}
+
+// func joinPathToURL(u *url.URL, elem ...string) *url.URL {
+// 	elem = append([]string{u.EscapedPath()}, elem...)
+// 	var p string
+// 	if !strings.HasPrefix(elem[0], "/") {
+// 		// Return a relative path if u is relative,
+// 		// but ensure that it contains no ../ elements.
+// 		elem[0] = "/" + elem[0]
+// 		p = path.Join(elem...)[1:]
+// 	} else {
+// 		p = path.Join(elem...)
+// 	}
+// 	// path.Join will remove any trailing slashes.
+// 	// Preserve at least one.
+// 	if strings.HasSuffix(elem[len(elem)-1], "/") && !strings.HasSuffix(p, "/") {
+// 		p += "/"
+// 	}
+// 	url := *u
+// 	url.setPath(p)
+// 	return &url
+// }
