f937a226a700d220c7709d474bda58e4c8fd193d
s-beats <sakashin10291029@gmail.com>
2022-10-03 21:49:03 +0900

    text template

--- examples/template/main.go
+++ examples/template/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"os"
 )
 
 func main() {
-	if _, err := template.ParseFiles("examples/template/data/hello.tmpl"); err != nil {
+	tmpl, err := template.ParseFiles("examples/template/data/hello.tmpl")
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	if err := tmpl.Execute(os.Stdout, map[string]any{
+		"Name": "beats",
+		"Age":  20,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Hello beats
+	// You are 20 years old.
 
 	// TODO:
 	// fs := os.DirFS("data")
