2925cb4fb5160dfc3ff967352c176c4eddaae6e1
s-beats <sakashin10291029@gmail.com>
2022-10-04 09:10:10 +0900

    parse file system

--- examples/template/main.go
+++ examples/template/main.go
@@ -23,11 +23,21 @@ func main() {
 	// You are 20 years old.
 
 	// TODO:
-	// fs := os.DirFS("data")
-	// tmpl := template.New("hello")
-	// _, err := tmpl.ParseFS(fs, "hello.tmpl")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	fs := os.DirFS("examples/template/data")
+	// ファイル名と揃える必要がある？
+	tmpl = template.New("hello.tmpl")
+	_, err = tmpl.ParseFS(fs, "hello.tmpl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := tmpl.Execute(os.Stdout, map[string]any{
+		"Name": "beats",
+		"Age":  20,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Hello beats
+	// You are 20 years old.
 }
