67dbe3575676bc2536dc850a533aa7ecab9ba1a5
s-beats <sakashin10291029@gmail.com>
2022-10-08 20:43:26 +0900

    search dir

--- examples/filepath/main.go
+++ examples/filepath/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func main() {
 	join()
 	walk()
 	glob()
+	dir()
 }
 
 func join() {
@@ -44,4 +47,25 @@ func glob() {
 	}
 	// examples/filepath/dummy.go
 	// examples/filepath/main.go
+}
+
+// ファイルを探してのディレクトリパスを取得する
+func dirInternal(file string) (string, bool) {
+	_, filename, _, _ := runtime.Caller(0)
+	fn := filepath.Dir
+	for dir := fn(filename); dir != "/" && dir != "."; dir = fn(dir) {
+		_, err := os.Stat(filepath.Join(dir, file))
+		if err == nil {
+			return dir, true
+		}
+	}
+	return "", false
+}
+
+func dir() {
+	s, ok := dirInternal(".gitignore")
+	if ok {
+		fmt.Println("---------- dir ----------")
+		fmt.Println(s)
+	}
 }
