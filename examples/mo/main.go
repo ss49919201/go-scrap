e73aaa794d96014d0a54cd45d262cfd42b107103
s-beats <sakashin10291029@gmail.com>
2022-10-21 01:12:27 +0900

    fp

--- examples/mo/main.go
+++ examples/mo/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/samber/lo"
 	"github.com/samber/mo"
 )
 
-func main() {
+func op() {
 	var ip *int
 	option1 := mo.Some(ip)
 	fmt.Println(option1.OrEmpty())                                                             // nil
@@ -33,4 +34,40 @@ func main() {
 	option1.Match(onValue, onNone).ForEach(func(value *int) { fmt.Println(*value) }) // 2
 
 	mo.None[*int]().Match(onValue, onNone).ForEach(func(value *int) { fmt.Println(*value) }) // 4
+}
+
+type user struct {
+	id int
+}
+
+func getUser(id int) mo.Result[*user] {
+	if id == 1 {
+		return mo.Ok(&user{1})
+	} else {
+		return mo.Err[*user](errors.New("user not found"))
+	}
+}
+
+func insertUser(u *user) mo.Result[*user] {
+	if u.id == 1 {
+		return mo.Ok(&user{})
+	} else {
+		return mo.Err[*user](errors.New("user not found"))
+	}
+}
+
+func validateUserFn() (func(*user) (*user, error), func(err error) (*user, error)) {
+	return func(u *user) (*user, error) { return u, nil },
+		func(err error) (*user, error) { return nil, err }
+}
+
+func createUserFn() (func(*user) (*user, error), func(err error) (*user, error)) {
+	return func(u *user) (*user, error) { return u, nil },
+		func(err error) (*user, error) { return nil, err }
+}
+
+func main() {
+	r := getUser(1)
+	r.Match(validateUserFn()).Match(createUserFn())
+	insertUser(r.MustGet())
 }
