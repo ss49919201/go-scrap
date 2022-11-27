53af15685f36085f55528546484d0a04006d995f
s-beats <sakashin10291029@gmail.com>
2022-11-27 10:52:32 +0900

    resty

--- examples/resty/main.go
+++ examples/resty/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 
@@ -10,57 +11,59 @@ import (
 
 func main() {
 	client := resty.New()
-	post(*client)
+	// post(*client)
 	get(*client)
 }
 
 func get(client resty.Client) {
 	// use raw body
-	res, err := client.R().
-		SetDoNotParseResponse(true).
-		Get("http://localhost:12345/example")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("res.Result() == nil", res.Result() == nil)
-	body := res.RawBody()
-	defer body.Close()
-	b, err := ioutil.ReadAll(body)
-	if err == nil {
-		fmt.Println("ioutil.ReadAll(body) => ", string(b))
-	} else {
-		fmt.Println("ioutil.ReadAll(body) => ", err)
-	}
-	buf := new(bytes.Buffer)
-	if _, err := buf.Write(b); err != nil {
-		fmt.Println("buf.Write(b) => ", err)
-	}
-	b2, err := ioutil.ReadAll(buf)
-	if err == nil {
-		fmt.Println("ioutil.ReadAll(buf)", string(b2))
-	} else {
-		fmt.Println("ioutil.ReadAll(buf)", err)
-	}
+	// res, err := client.R().
+	// 	SetDoNotParseResponse(true).
+	// 	Get("http://localhost:12345/example")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("res.Result() == nil", res.Result() == nil)
+	// body := res.RawBody()
+	// defer body.Close()
+	// b, err := ioutil.ReadAll(body)
+	// if err == nil {
+	// 	fmt.Println("ioutil.ReadAll(body) => ", string(b))
+	// } else {
+	// 	fmt.Println("ioutil.ReadAll(body) => ", err)
+	// }
+	// buf := new(bytes.Buffer)
+	// if _, err := buf.Write(b); err != nil {
+	// 	fmt.Println("buf.Write(b) => ", err)
+	// }
+	// b2, err := ioutil.ReadAll(buf)
+	// if err == nil {
+	// 	fmt.Println("ioutil.ReadAll(buf)", string(b2))
+	// } else {
+	// 	fmt.Println("ioutil.ReadAll(buf)", err)
+	// }
 
 	// use parsed response
 	type tmp struct {
 		String string `json:"String"`
 	}
 	t := &tmp{}
-	res, err = client.R().
+	_, err := client.R().
 		SetResult(t).
 		Get("http://localhost:12345/example")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("res.Result() == nil", res.Result() == nil)
-	t2 := res.Result().(*tmp)
-	fmt.Printf("res.Result().(*tmp) => %#v\n", t2)
-	b, err = ioutil.ReadAll(res.RawBody())
+	fmt.Println(t)
+	encoded := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(encoded).Encode(t); err != nil {
+		fmt.Println("json.NewEncoder(encoded).Encode(t) => ", err)
+	}
+	encodedBytes, err := ioutil.ReadAll(encoded)
 	if err == nil {
-		fmt.Println("ioutil.ReadAll(res.RawBody()) => ", string(b))
+		fmt.Println("ioutil.ReadAll(encoded) => ", string(encodedBytes))
 	} else {
-		fmt.Println("ioutil.ReadAll(res.RawBody()) => ", err) // http: read on closed response body
+		fmt.Println("ioutil.ReadAll(encoded) => ", err)
 	}
 }
 
