81ec40cdbe7559f02f93945777f71a8d1dcf0f9b
s-beats <sakashin10291029@gmail.com>
2022-11-29 19:31:00 +0900

    benchmark

--- examples/benchmark/map_store_vs_slice_append_test.go
+++ examples/benchmark/map_store_vs_slice_append_test.go
@@ -1,39 +1,51 @@
 package benchmark
 
 import (
-	"math"
 	"testing"
 )
 
-var n = int(math.Pow10(6))
+var n int = 1e6
 
-// 0.2221 ns/op
 func BenchmarkMapStore(b *testing.B) {
 	b.ResetTimer()
-	m := make(map[int]int)
-	for i := 0; i < n; i++ {
-		m[i] = i
+	for i := 0; i < b.N; i++ {
+		m := make(map[int]int)
+		for i := 0; i < n; i++ {
+			m[i] = i
+		}
 	}
 }
 
-// 0.01210 ns/op
-func BenchmarkSliceAppend(b *testing.B) {
+func BenchmarkSliceAppendA(b *testing.B) {
 	b.ResetTimer()
-	s := []int{}
-	for i := 0; i < n; i++ {
-		s = append(s, i)
+	for i := 0; i < b.N; i++ {
+		s := []int{}
+		for i := 0; i < n; i++ {
+			s = append(s, i)
+		}
 	}
 }
 
-// 0.001994 ns/op
+func BenchmarkSliceAppendB(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s := make([]int, 0, n)
+		for i := 0; i < n; i++ {
+			s = append(s, i)
+		}
+	}
+}
+
 func BenchmarkSliceBeforeInit(b *testing.B) {
 	b.ResetTimer()
-	s := make([]int, n)
-	for i := 0; i < n; i++ {
-		s[i] = -1
-	}
+	for i := 0; i < b.N; i++ {
+		s := make([]int, n)
+		for i := 0; i < n; i++ {
+			s[i] = i
+		}
 
-	for i := 0; i < n; i++ {
-		s[i] = i
+		for i := 0; i < n; i++ {
+			s[i] = i
+		}
 	}
 }
