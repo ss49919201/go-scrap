c5b416553836f14525f49dfe845aabd34dec391f
s-beats <sakashin10291029@gmail.com>
2023-01-22 15:35:11 +0900

    :sparkles: bubble sort

---
+++ sort_algorithm/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBubble(t *testing.T) {
+	t.Run("buble sort", func(t *testing.T) {
+		assert.Equal(t, []int{1, 2, 3, 4, 5}, bubble([]int{3, 1, 4, 5, 2}))
+	})
+}
