df15b7b5b4dfb6fe3211a384ef97a8e64cf95f42
ss49919201 <sakashin10291029@gmail.com>
2023-02-23 00:33:48 +0900

    refactor custom error

--- error/internal/error.go
+++ error/internal/error.go
@@ -5,32 +5,37 @@ import (
 	"runtime"
 )
 
-type CustomError struct {
+type CustomError interface {
+	error
+	Stack() string
+}
+
+type customError struct {
 	msg   string
 	stack string
 }
 
-func (c *CustomError) Error() string {
+func (c *customError) Error() string {
 	return c.msg
 }
 
-func (c *CustomError) Stack() string {
+func (c *customError) Stack() string {
 	return c.stack
 }
 
-func NewCustomError(s string) error {
+func New(s string) error {
 	return newCustomError(s, 1)
 }
 
-func WrapCustomError(err error) error {
+func WithStack(err error) error {
 	return newCustomError(err.Error(), 1)
 }
 
-func newCustomError(s string, skip int) *CustomError {
+func newCustomError(s string, skip int) CustomError {
 	pc, f, l, _ := runtime.Caller(skip + 1)
 	fn := runtime.FuncForPC(pc)
 	stack := fmt.Sprintf("%s %s: %d", fn.Name(), f, l)
-	return &CustomError{
+	return &customError{
 		msg:   s,
 		stack: stack,
 	}
--- error/main.go
+++ error/main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	err1 := errors.New("error")
-	err2 := &internal.CustomError{}
-	err3 := fmt.Errorf("%w", &internal.CustomError{})
+	var err2 internal.CustomError
+	err3 := fmt.Errorf("%w", internal.New(""))
 
 	fmt.Println(errors.Is(err1, err2))  // false
 	fmt.Println(errors.As(err1, &err2)) // false
@@ -21,10 +21,10 @@ func main() {
 	fmt.Println(errors.Is(err3, err2))  // true
 	fmt.Println(errors.As(err3, &err2)) // true
 
-	err4 := internal.NewCustomError("custom")
-	err5 := internal.WrapCustomError(errors.New("wrapped"))
+	err4 := internal.New("custom")
+	err5 := internal.WithStack(errors.New("wrapped"))
 	for _, err := range []error{err4, err5} {
-		var ce *internal.CustomError
+		var ce internal.CustomError
 		if errors.As(err, &ce) {
 			fmt.Printf("error = %s, stack = %s\n", ce.Error(), ce.Stack())
 		}
