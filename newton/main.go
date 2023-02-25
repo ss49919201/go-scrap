970d1fee9295ec52c53cc26a01a90f8aa7795f0d
ss49919201 <sakashin10291029@gmail.com>
2023-02-25 15:39:19 +0900

    ニュートン法

--- newton/main.go
+++ newton/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -15,16 +14,15 @@ func solve() {
 	r := 2.0
 	a := 2.0
 
-	// y=x^2の式
-	f := func(n float64) float64 { return math.Pow(n, 2) }
-	// 導関数
-	fDash := func(n float64) float64 { return 2 * n }
-
 	for i := 0; i < 10; i++ {
-		// 直線の方程式より接線の方程式を求める
-		// y-b=m(x-a) -> x = (y - b + ma) / m
-		// aを交点のx座標に上書きする
-		a = (r - f(a) + (fDash(a) * a)) / fDash(a)
+		zahyouX := a
+		zahyouY := a * a
+
+		// 接線の方程式を求める
+		katamuki := 2.0 * zahyouX            // 傾き
+		seppen := zahyouY - katamuki*zahyouX // 切片
+		// 求めた接線の方程式のyにrを代入することで、y=rとの交点のx座標を求める
+		a = (r - seppen) / katamuki
 		fmt.Println(a)
 	}
 }
