cca63a6bd490825adb52a7f1f0bf1f7e91faced9
ss49919201 <sakashin10291029@gmail.com>
2023-07-19 00:07:55 +0900

    shallow copy

--- copy/main.go
+++ copy/main.go
@@ -1,7 +1,30 @@
 package main
 
-func shallow() {}
+import "fmt"
+
+type s struct {
+	n  int
+	np *int
+}
+
+func shallow(src *s) *s {
+	return src
+}
 
 func deep() {}
 
-func main() {}
+func main() {
+	a := new(s)
+	b := new(s)
+	a.n = 1
+	b.n = 2
+	fmt.Println(a == b)     // false
+	fmt.Println(*a == *b)   // false
+	fmt.Println(a.n == b.n) // false
+
+	b = shallow(a)
+	b.n = 3
+	fmt.Println(a == b)     // true
+	fmt.Println(*a == *b)   // true
+	fmt.Println(a.n == b.n) // true
+}
